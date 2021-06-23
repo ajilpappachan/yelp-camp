131a13456eef1d1cbf9dc2c1bfb1fc3315a43c67
Ajil Pappachan <ajilpappachan@gmail.com>
2021-06-23 23:10:16 +0530

    Pushing to Heroku

--- index.js
+++ index.js
@@ -165,6 +165,7 @@ app.use((err, req, res, next) => {
 	res.status(status).render("error", { err });
 });
 
-app.listen(3000, () => {
-	console.log("Server is running on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
 });
